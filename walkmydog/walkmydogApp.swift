087c6fbc04618237cef467a72bc52640d064b296
Doris <doris070714@gmail.com>
2024-12-10 10:51:14 +0800

    Initial Commit

---
+++ walkmydog/walkmydogApp.swift
@@ -0,0 +1,17 @@
+//
+//  walkmydogApp.swift
+//  walkmydog
+//
+//  Created by App_team on 2024/12/10.
+//
+
+import SwiftUI
+
+@main
+struct walkmydogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ walkmydogTests/walkmydogTests.swift
@@ -0,0 +1,17 @@
+//
+//  walkmydogTests.swift
+//  walkmydogTests
+//
+//  Created by App_team on 2024/12/10.
+//
+
+import Testing
+@testable import walkmydog
+
+struct walkmydogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
